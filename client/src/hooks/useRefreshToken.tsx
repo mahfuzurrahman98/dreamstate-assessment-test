10fac19962760d608b1c022091d530a3223c4687
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-01-27 14:02:28 +0600

    spacing format fixed[2 to 4]

--- client/src/hooks/useRefreshToken.tsx
+++ client/src/hooks/useRefreshToken.tsx
@@ -2,26 +2,26 @@ import { axiosPrivate } from '../api/axios';
 import useAuth from './useAuth';
 
 const useRefreshToken = () => {
-  const { setAuth } = useAuth();
+    const { setAuth } = useAuth();
 
-  const refresh = async () => {
-    try {
-      const response = await axiosPrivate.post('/users/auth/refreshtoken');
-      // console.log(response);
-      const data = response.data.data;
+    const refresh = async () => {
+        try {
+            const response = await axiosPrivate.post('/users/refresh-token');
+            console.log('refresh: ', response);
+            const data = response.data.data;
 
-      setAuth({
-        token: data.access_token,
-      });
+            setAuth({
+                token: data.accessToken,
+            });
 
-      return data.access_token;
-    } catch (error) {
-      console.log('error from useRefreshToken.tsx', error);
-      throw error;
-    }
-  };
+            return data.accessToken;
+        } catch (error) {
+            console.log('error from useRefreshToken.tsx', error);
+            throw error;
+        }
+    };
 
-  return refresh;
+    return refresh;
 };
 
 export default useRefreshToken;
--- client/src/pages/RootLayout.tsx
+++ client/src/pages/RootLayout.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from 'react';
 import Header from '../components/Header';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <div className="max-w-6xl w-full mt-5 flex flex-col mx-auto px-4">
-        {children}
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <div className="max-w-6xl w-full mt-5 flex flex-col mx-auto px-4">
+                {children}
+            </div>
+        </div>
+    );
 };
 
 export default RootLayout;
