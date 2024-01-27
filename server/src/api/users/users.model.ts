5ed67de8e8c6510165554e100f6fd9b32ba69375
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-01-27 16:27:02 +0600

    added try catch to avoid errors

--- server/src/api/users/users.model.ts
+++ server/src/api/users/users.model.ts
@@ -1,42 +1,54 @@
-import { Mongoose } from 'mongoose';
+import { Document, Model, Mongoose, Schema } from 'mongoose';
 import Database from '../../configs/Database';
 import { IUser } from '../../interfaces/user';
+import CustomError from '../../utils/CustomError';
 
-const db: Mongoose = Database.getInstance();
+let db: Mongoose;
+
+try {
+    db = Database.getInstance();
+} catch (error: any) {
+    throw new CustomError(500, error.message);
+}
 
 const collectionName = 'users';
-const schema = new db.Schema<IUser>(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: false,
-        },
-        google_auth: {
-            type: Boolean,
-            required: false,
-        },
-        active: {
-            type: Boolean,
-            required: true,
-        },
-        deletedAt: {
-            type: Date,
-            default: null,
-        },
-    },
-    { timestamps: true }
-);
+let userModel: Model<IUser & Document>;
 
-const userModel =
-    db.models[collectionName] || db.model<IUser>(collectionName, schema);
+try {
+    const schema = new Schema<IUser>(
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            email: {
+                type: String,
+                required: true,
+                unique: true,
+            },
+            password: {
+                type: String,
+                required: false,
+            },
+            googleAuth: {
+                type: Boolean,
+                required: false,
+            },
+            active: {
+                type: Boolean,
+                required: true,
+            },
+            deletedAt: {
+                type: Date,
+                default: null,
+            },
+        },
+        { timestamps: true }
+    );
+
+    userModel = db.models[collectionName] || db.model<IUser & Document>(collectionName, schema);
+} catch (error: any) {
+    throw new CustomError(500, error.message);
+}
 
 export default userModel;
