6dc76279c2b5f2c7cb9895254d38557b99304f34
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-01-27 18:33:43 +0600

    loading spinner added

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,11 +1,9 @@
-import PageLoad from '../assets/future-penguin.gif';
-
 const Loading = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <img src={PageLoad} alt="" />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="w-24 h-24 border-8 border-dashed rounded-full animate-spin border-amber-800"></div>
+        </div>
+    );
 };
 
 export default Loading;
